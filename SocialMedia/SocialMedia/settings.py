71572e97d4517f7ca2af85efa8c7cf1352bf751a
Nikhil <nikhil22322@iiitd.ac.in>
2025-02-25 01:41:41 +0530

    added mysql server support

--- SocialMedia/SocialMedia/settings.py
+++ SocialMedia/SocialMedia/settings.py
@@ -73,11 +73,16 @@ WSGI_APPLICATION = 'SocialMedia.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+import pymysql
+pymysql.install_as_MySQLdb()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'socialmediadb',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',  
+        'PORT': '3306',
     }
 }
 
