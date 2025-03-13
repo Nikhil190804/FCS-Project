c6b6d53785d65388866b43b806300766cab14702
Nikhil <nikhil22322@iiitd.ac.in>
2025-03-14 02:09:05 +0530

    added otp script with virtual keyboard

---
+++ SocialMedia/static/Users/otp.js
@@ -0,0 +1,31 @@
+let Keyboard = window.SimpleKeyboard.default;
+
+let keyboard = new Keyboard({
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button)
+});
+
+/**
+ * Update simple-keyboard when input is changed directly
+ */
+document.querySelector(".input").addEventListener("input", event => {
+  keyboard.setInput(event.target.value);
+});
+
+
+function onChange(input) {
+  document.querySelector(".input").value = input;
+}
+
+function onKeyPress(button) {
+  if (button === "{shift}" || button === "{lock}") handleShift();
+}
+
+function handleShift() {
+  let currentLayout = keyboard.options.layoutName;
+  let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+  keyboard.setOptions({
+    layoutName: shiftToggle
+  });
+}
