7740b3104f88bf7953b783611108a96fe90d8bb3
dhruv22162 <dhruv22162@iiitd.ac.in>
2025-03-21 17:22:25 +0530

    User verification by admin

--- SocialMedia/SocialMedia/settings.py
+++ SocialMedia/SocialMedia/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'socialmediadb',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'root',
         'HOST': 'localhost',  
         'PORT': '3306',
     }
