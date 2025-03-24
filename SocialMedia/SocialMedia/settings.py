5d1e42dd3ffdfb9a1944237885a563f62fb91c2a
pandillapelly22345 <pandillapelly22345@iiitd.ac.in>
2025-03-24 22:35:44 +0530

    marketplace

--- SocialMedia/SocialMedia/settings.py
+++ SocialMedia/SocialMedia/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+import environ
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$#0w$fx@#&z$*xsxnu!4xftm5f87ivo8vk(7rf)_^r0^gz2crr'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -38,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Users',
+    'MarketPlace'
 ]
 
 MIDDLEWARE = [
@@ -82,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'socialmediadb',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'root123',
         'HOST': 'localhost',  
         'PORT': '3306',
     }
@@ -155,3 +162,8 @@ EMAIL_USE_TLS = True  # Set to False if using SSL (port 465)
 EMAIL_HOST_USER = "the404s.fcs@gmail.com"
 EMAIL_HOST_PASSWORD = "xevy dlww thky aomt"  # Use an app password (not your real password!)
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+STRIPE_PUBLIC_KEY=env('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY=env('STRIPE_SECRET_KEY')
+STRIPE_WEBHOOK_SECRET=env('STRIPE_WEBHOOK_SECRET')
