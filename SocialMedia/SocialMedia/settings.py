a50754ae8c243a481a6220f24a5da96d4845badd
Nikhil <nikhil22322@iiitd.ac.in>
2025-03-29 23:27:23 +0530

    merge conflict

--- SocialMedia/SocialMedia/settings.py
+++ SocialMedia/SocialMedia/settings.py
@@ -13,12 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
-# import environ
-# env = environ.Env(
-#     DEBUG=(bool, False)
-# )
-# environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
