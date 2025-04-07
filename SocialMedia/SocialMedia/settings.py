843dcb396fca105d98b8a06257c3f6abc45d4107
Nikhil <nikhil22322@iiitd.ac.in>
2025-04-07 17:23:18 +0530

    new middleware config

--- SocialMedia/SocialMedia/settings.py
+++ SocialMedia/SocialMedia/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Users.middleware.AuthenticationMiddleware',
+    'Users.middleware.LegitAccessMiddleware',
     'Users.middleware.SignupProcessMiddleware',
 ]
 
